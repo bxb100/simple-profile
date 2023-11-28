ac6fd682bbbe8c2ce3dda21a8ce720f0cf3d46da
Lemon <bxb100@outlook.com>
2023-11-28 23:39:17 +0800

    chore: add star field

--- src/App.vue
+++ src/App.vue
@@ -24,52 +24,63 @@ onMounted(() => {
       .pauseFor(2000)
       .start();
 })
+
+const app = ref()
+
+function toggleStarField() {
+  app.value.classList.toggle('show')
+}
+
+
 </script>
 
 <template>
-  <main class=" min-h-screen dark:bg-gray-900 flex flex-col place-content-center">
-    <section class="text-gray-600 body-font dark:text-gray-300 w-full">
-      <div class="container px-5 py-24 mx-auto">
-        <div class="xl:w-1/2 lg:w-3/4 w-full mx-auto text-center">
-          <picture>
-            <source srcset="./assets/dog.webp" type="image/webp">
-            <source srcset="./assets/dog.png" type="image/jpeg">
-            <img class="rounded-b-full w-20 h-20 object-center inline-block mb-6 border-gray-200"
-                 src="./assets/dog.png" alt="dog head">
-          </picture>
-          <p class="leading-relaxed text-lg">
-            Hello <span class="wave">👋</span>,
-            <span ref="el"></span>
-          </p>
-          <!--<p> <span>不闻不若闻之，闻之不若见之，见之不若知之，知之不若行之。学至于行之而止矣。</span></p>
-          <p class="invisible"><span>《荀子·儒效》</span></p>-->
-          <span class="inline-block h-1 w-10 rounded bg-indigo-500 mt-8 mb-6"></span>
-          <h2 class="text-gray-900 dark:text-gray-300  font-medium title-font tracking-wider text-sm">XIAOBO</h2>
-          <p class="text-gray-500 dark:text-gray-400">Developer</p>
-          <hr class=" m-8 ">
-          <p class="text-center tracking-normal text-slate-500 dark:text-slate-400">
-            Mail me at <a class=" hover:underline" href="mailto:xiaobo@tomcat.run">
-            xiaobo@tomcat.run
-          </a>
-          </p>
-          <p class=" text-center tracking-normal text-slate-500 dark:text-slate-400">
-            Find me on
-            <a class="hover:underline font-bold decoration-blue-500"
-               href="https://blog.tomcat.run">Blog</a>
-            /
-            <a class="hover:underline font-bold decoration-sky-500"
-               href="https://github.com/bxb100">GitHub</a>
-          </p>
+  <main class="min-h-screen dark:bg-gray-900">
+    <div id="app" ref="app" class="min-h-screen flex flex-col place-content-center">
+      <section class="text-gray-600 body-font dark:text-gray-300 w-full">
+        <div class="container px-5 py-24 mx-auto">
+          <div class="xl:w-1/2 lg:w-3/4 w-full mx-auto text-center">
+            <picture @mouseenter="toggleStarField" @mouseleave="toggleStarField">
+              <source srcset="./assets/dog.webp" type="image/webp">
+              <source srcset="./assets/dog.png" type="image/jpeg">
+              <img class="rounded-b-full w-20 h-20 object-center inline-block mb-6 border-gray-200"
+                   src="./assets/dog.png" alt="dog head">
+            </picture>
+            <p class="leading-relaxed text-lg">
+              Hello <span class="wave">👋</span>,
+              <span ref="el"></span>
+            </p>
+            <!--<p> <span>不闻不若闻之，闻之不若见之，见之不若知之，知之不若行之。学至于行之而止矣。</span></p>
+            <p class="invisible"><span>《荀子·儒效》</span></p>-->
+            <span class="inline-block h-1 w-10 rounded bg-indigo-500 mt-8 mb-6"></span>
+            <h2 class="text-gray-900 dark:text-gray-300  font-medium title-font tracking-wider text-sm">XIAOBO</h2>
+            <p class="text-gray-500 dark:text-gray-400">Developer</p>
+            <hr class=" m-8 ">
+            <p class="text-center tracking-normal text-slate-500 dark:text-slate-400">
+              Mail me at <a class=" hover:underline" href="mailto:xiaobo@tomcat.run">
+              xiaobo@tomcat.run
+            </a>
+            </p>
+            <p class=" text-center tracking-normal text-slate-500 dark:text-slate-400">
+              Find me on
+              <a class="hover:underline font-bold decoration-blue-500"
+                 href="https://blog.tomcat.run">Blog</a>
+              /
+              <a class="hover:underline font-bold decoration-sky-500"
+                 href="https://github.com/bxb100">GitHub</a>
+            </p>
+          </div>
         </div>
-      </div>
-    </section>
-    <footer class="absolute bottom-1 left-0 right-0 w-36 mx-auto" aria-labelledby="footer-heading">
-      <h2 id="footer-heading" class="sr-only">Footer</h2>
-      <div class="flex justify-center">
-        <a class="text-xs leading-5 text-gray-500 dark:text-gray-400 hover:text-indigo-500  "
-           href="https://beian.miit.gov.cn/" target="_blank">皖ICP备2023022702号</a>
-      </div>
-    </footer>
+      </section>
+      <footer class="absolute bottom-1 left-0 right-0 w-36 mx-auto" aria-labelledby="footer-heading">
+        <h2 id="footer-heading" class="sr-only">Footer</h2>
+        <div class="flex justify-center">
+          <a class="text-xs leading-5 text-gray-500 dark:text-gray-400 hover:text-indigo-500  "
+             href="https://beian.miit.gov.cn/" target="_blank">皖ICP备2023022702号</a>
+        </div>
+      </footer>
+    </div>
+
   </main>
 </template>
 <style>
@@ -120,4 +131,16 @@ onMounted(() => {
     transform: rotate(0.0deg)
   }
 }
+
+/*
+https://twitter.com/KaAnDK/status/1729149373043610098
+https://codepen.io/amit_merchant/pen/MWPbaVG
+ */
+#app.show {
+  height: 100%;
+  background-size: 100% 100% !important;
+  background: url("assets/starfield.svg") no-repeat center;
+}
+
+
 </style>
